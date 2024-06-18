ebc9a4038b21bb26af115fbc58c7b76c53ff9ebb
Shadi J Sbaih <shadisbaih20014@gmail.com>
2024-06-18 15:15:31 +0300

    first version -menu bar fixed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('navbar');
+let navbar = document.querySelector('.navbar');
 
 menu.onclick=() => {
     navbar.classList.toggle('active');
