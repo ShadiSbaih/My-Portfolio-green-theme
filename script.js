4e111c29f1d7d376907df69bed70f3b291eb96e6
Shadi J Sbaih <shadisbaih20014@gmail.com>
2024-06-18 15:07:37 +0300

    first version

---
+++ script.js
@@ -0,0 +1,22 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('navbar');
+
+menu.onclick=() => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('bx-x');
+}
+window.onscroll=() => {
+    navbar.classList.remove('active');
+    menu.classList.remove('bx-x');
+}
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer','Javascript Developer','Web Developer','UI/UX Developer'],
+    typeSpeed: 80,
+    backSpeed:80,
+    backDelay:1200,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true,
+    cursorChar: '/>',
+    smartBackspace: true,
+  });
